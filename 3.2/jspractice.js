54c6c21fac4891a8ed53073e9afe138f11fb7050
Laura <41702570+SuitcaseCoder@users.noreply.github.com>
2021-03-03 07:30:09 -0600

    3.2 jspractice

--- 3.2/jspractice.js
+++ 3.2/jspractice.js
@@ -8,3 +8,51 @@
 // Some of those key:value pairs are organized by what they can be used for
     // // for example, array functions, are just regular functions that we execute by chaining it to an array, so it grabs the entire array, loops through each item and executes some code. 
     // there are different types of functions/methods available to us that are stored in the 'java
+
+
+let js = {
+    arrayFunctions:
+        [
+            // iterate through arrays
+            {map: function(){}},
+            // iterate through arrays but return something based on a conditional
+            {filter: function(){}}
+        
+        ],
+    methods: [
+        {},
+    ]
+}
+
+// // PRACTICE ARRAY FUNCTIONS
+let animals = ['birds','dogs','cats','bears','lions', 'monkeys']; //array // [element, element, element, element]
+
+// I want to map through the animals list
+let sentences = animals.map(
+    // I'm gonna provide 'map' a function -telling it what to do at each iteration
+    // // that function is going to require a parameter (which you get to name) 
+    // // the argument being passed in will ALWAYS be whichever element in the array that the 'loop'/iteration is currently on
+    function(element){
+
+        // console.log(element);
+        return `${element} are found at the zoo.`;
+    }
+    // map will take the result of the above function and create a new array with it.
+);
+
+console.log(sentences);
+
+// functions take in arguments but use parameters until that argument is passed in
+function myFunction(parameter, placeHolderName, willBeReplaced){
+    return `hello, ${parameter}, today we're talking about: ${placeHolderName}, ..... ${willBeReplaced}`;
+}
+
+console.log(myFunction('susan', 'javascript', 'ladeeda'));
+console.log(myFunction('tom', 'html', 'wooohoo'));
+
+
+function writeOnBoard(student){
+    // write on board
+}
+
+writeOnBoard('billy');
