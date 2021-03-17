9f2afddd91dd5c8b7ce842e085828b77e8765d46
Laura <41702570+SuitcaseCoder@users.noreply.github.com>
2021-03-17 12:06:32 -0500

    5.3 / notes

---
+++ 5.3/notes/react-day-2/src/App.js
@@ -0,0 +1,26 @@
+import React from 'react' 
+import './App.css';
+
+// functional component
+const Count=()=>{
+  // react hook example. 
+  // // count is setting a new key/value pair in our 'state' 
+  // // setCount can be called on anytime we need to update the state
+  // // we're setting it to 0 as a starting point
+  let [count, setCount] = React.useState(0);
+
+  const handleClick=()=>{
+    setCount(count+1);
+  }
+
+  return(
+    <div>
+      <div>{count}</div>
+      <button onClick={handleClick}>
+        Increment
+      </button>
+    </div>
+  )
+}
+
+export default Count;
